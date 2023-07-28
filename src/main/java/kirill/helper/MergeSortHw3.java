7823acaf7a7436f6f17108eef3d6b75ccef08991
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-07-28 21:19:29 +0300

    sort task update/ merge sort added

--- src/main/java/kirill/helper/MergeSortHw3.java
+++ src/main/java/kirill/helper/MergeSortHw3.java
@@ -4,8 +4,42 @@ import java.util.Arrays;
 
 public class MergeSortHw3 {
 
-    public static int[] mergeSort(int[] a){
-        Arrays.sort(a);
-        return a;
+    public static int[] mergeSort(int[] array) {
+
+        if (array.length < 2) {
+            return array;
+        }
+        int mid = array.length / 2;
+        int[] leftSideArray = new int[mid];
+        int[] rightSideArray = new int[array.length - mid];
+
+        for (int i = 0; i < mid; i++) {
+            leftSideArray[i] = array[i];
+        }
+        for (int j = 0; j < array.length - mid; j++) {
+            rightSideArray[j] = array[mid + j];
+        }
+        mergeSort(leftSideArray);
+        mergeSort(rightSideArray);
+
+        mergeArrays(array, leftSideArray, rightSideArray, mid, array.length - mid);
+        return array;
+    }
+
+    public static void mergeArrays(int[] array,int[] leftSideArray,int[] rightSideArray, int endLeftSide, int endRightSide){
+        int i = 0,j = 0,k = 0;
+        while(i<endLeftSide && j<endRightSide){
+            if(leftSideArray[i] <= rightSideArray[j]){
+                array[k++] = leftSideArray[i++];
+            }else{
+                array[k++] = rightSideArray[j++];
+            }
+        }
+        while(i <endLeftSide){
+            array[k++] = leftSideArray[i++];
+        }
+        while(j< endRightSide){
+            array[k++] = rightSideArray[j++];
+        }
     }
 }
